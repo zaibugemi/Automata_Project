db023953e9d5e05f90af7d879834d3660533daaa
zaibugemi <2016n1326@gmail.com>
2020-05-17 08:55:26 +0500

    EASY PART DONE APART FROM (INCREMENT and DECREMENT)

--- interpreter.py
+++ interpreter.py
@@ -3,6 +3,7 @@ from parser import *
 import ply.lex as lex
 import ply.yacc as yacc
 import re
+import sys
 
 
 
@@ -10,8 +11,10 @@ import re
 mylex = lex.lex(module=lexer)
 parser = yacc.yacc()
 
-code = '''INT z = 2; DOUBLE fl = 2.8; CHAR a = "b"; z = 4; z = 6; z = 8; a = "k"; a = "w";fl = 2.999999;z; a;fl'''
+#code = '''INT z = 2; DOUBLE fl = 2.8; CHAR a = "b"; z = 4; z = 6; z = 8; a = "k"; a = "w";fl = 2.999999;z; a;fl'''
 
+f = open(sys.argv[1],'r')
+code = f.read()
 ptree = parser.parse(code)
 # print(ptree)
 
